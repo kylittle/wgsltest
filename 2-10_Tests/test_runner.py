7b8f6167b12c56916d278ddea0b2671e5580b3b6
kylittle <kyelittl@ucsc.edu>
2023-02-22 02:30:38 -0800

    Fixed test script

--- 2-10_Tests/test_runner.py
+++ 2-10_Tests/test_runner.py
@@ -2,6 +2,7 @@ import time
 import signal
 import json
 import subprocess
+import random
 from termcolor import colored 
 
 class Timer:
@@ -64,22 +65,79 @@ def test_case(iteration):
     # Insert threading
     print('\nInserting threading...')
     timer.start()
-    subprocess.run(['wgslsmith-thread', '-w', '32', clean_file, clean_file])
+    # TODO: Fix overhead and increase threads
+    subprocess.run(['wgslsmith-thread', '-w', '16', clean_file, clean_file])
     timer.stop()
 
     # Run and pipe output to file clean.out (clean up extra lines here) NOTE: Assumption is that the last two steps don't change
     # execution time
     print('\nGetting baseline output...')
     timer.start()
-    clean_out = open('clean.out', 'w')
-    subprocess.run(['wgslsmith-harness', 'run', '-c', 'dawn:vk:7857', clean_file], stdout=clean_out)
+
+    out = subprocess.run(['wgslsmith-harness', 'run', '--timeout', '10', '-c', 'dawn:vk:7857', clean_file], capture_output=True, text=True)
+    if 'timeout' in out.stdout:
+        print(colored('\tSlow Kernel', 'yellow'))
+        subprocess.run(['rm', clean_file]) # Clean up
+        timer.stop()
+        return
+    with open('clean.out', 'w') as f:
+        f.write(out.stdout)
+
+    print(colored('\tKernel OK continuing', 'green'))
     timer.stop()
 
     # Insert UB
+    print('\nInserting undefined behavior...')
+    timer.start()
+    subprocess.run(['wgslsmith-ub', '-c', '20', clean_file, ub_file])
+    # Write the UB json file here (TODO: Make this smarter)
+    low_bound = random.randint(65, 127)
+    up_bound = random.randint(127,132)
+    json_data = f'{{\n"0:0": [{low_bound},0,0,0,{up_bound},0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]\n}}'
+    with open(json_file, 'w') as f:
+        f.write(json_data)
+    timer.stop()
+
 
     # Run and pipe output to file ub.out (clean up extra lines here) TODO: We might be interested in the data stored in 0:4
+    print('\nGetting UB output...')
+    timer.start()
 
-    # Use Linux diff tool and save mismatches, delete non-mismatches
+    out = subprocess.run(['wgslsmith-harness', 'run', '--timeout', '10', '-c', 'dawn:vk:7857', ub_file], capture_output=True, text=True)
+    if 'timeout' in out.stdout:
+        print(colored('\tSlow Kernel', 'yellow'))
+        subprocess.run(['rm', clean_file]) # Clean up
+        subprocess.run(['rm', ub_file])
+        timer.stop()
+        return
+    with open('ub.out', 'w') as f:
+        f.write(out.stdout)
+
+    print(colored('\tKernel OK continuing', 'green'))
+    timer.stop()
+
+    # For now use Grep, lets get timing on this tomorrow (TODO)
+    print('\nComparing outputs...')
+    timer.start()
+    clean_out1 = subprocess.run(['grep', '0:1', 'clean.out'], capture_output=True, text=True).stdout
+    clean_out2 = subprocess.run(['grep', '0:2', 'clean.out'], capture_output=True, text=True).stdout
+    ub_out1 = subprocess.run(['grep', '0:1', 'ub.out'], capture_output=True, text=True).stdout
+    ub_out2 = subprocess.run(['grep', '0:2', 'ub.out'], capture_output=True, text=True).stdout
+
+    if clean_out1 != ub_out1 or clean_out2 != ub_out2:
+        # Mismatch
+        subprocess.run(['mkdir', f'mismatch{iteration}'])
+        # Move all relevant files to mismatch
+        subprocess.run(['mv', clean_file, ub_file, json_file, 'clean.out', 'ub.out', f'mismatch{iteration}'])
+        # Move mismatch to flagged
+        subprocess.run(['mv', f'mismatch{iteration}', 'flagged'])
+        print(colored('\tMismatch', 'red')) 
+    else:
+        # Clean up
+        print(colored('\tOk, cleaning up', 'green'))
+        subprocess.run(['rm', clean_file, ub_file, json_file, 'clean.out', 'ub.out'])
+
+    timer.stop()
 
 # TODO: Keep track of execution stats (would be nice to know how many timeouts we get so we can optimize the timeout bounds)
 if __name__ == "__main__":
@@ -99,8 +157,6 @@ if __name__ == "__main__":
         total_timer.start()
         test_case(iteration)
         print('\nTotal test time is: ', end='')
-        total_timer.stop()
-        # Do tests
 
     # Exited loop gracefully save state here
     fp = open('.state.json', 'w')
